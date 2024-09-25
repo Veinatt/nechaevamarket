4051937712f917e71ace2b063698e4a3ed8f41b0
Муравьёв Д.С. <C:\Users\muraviev\AppData\Roaming\The Bat!>
2024-09-25 17:31:40 +0300

    2509 1730

--- src/js/app.js
+++ src/js/app.js
@@ -72,313 +72,323 @@ ready(function () {
       }
     }, 1000);
   })
-  $(function () {
-    // setting
-    var debug = false;
+  
+  // $(function () {
+  //   // setting
+  //   var debug = false;
 
-    // fake ajax request
-    // $.mockjax({
-    //   url: '/api/tfa',
-    //   dataType: 'json',
-    //   response: function (settings) {
-    //     this.responseText = {
-    //       "ok": (Math.random() >= 0.5) // random true/false
-    //     };
-    //   },
-    //   responseTime: 1000
-    // });
+  //   // fake ajax request
+  //   // $.mockjax({
+  //   //   url: '/api/tfa',
+  //   //   dataType: 'json',
+  //   //   response: function (settings) {
+  //   //     this.responseText = {
+  //   //       "ok": (Math.random() >= 0.5) // random true/false
+  //   //     };
+  //   //   },
+  //   //   responseTime: 1000
+  //   // });
 
-    // pincode
-    var _pincode = [],
-    _req = null;
+  //   // pincode
+  //   var _pincode = [],
+  //   _req = null;
 
-    // main form
-    var $form = $('#form');
+  //   // main form
+  //   var $form = $('#form');
 
-    // pincode group
-    var $group = $form.find('.form__pincode');
+  //   // pincode group
+  //   var $group = $form.find('.form__pincode');
 
-    // all input fields
-    var $inputs = $group.find(':input');
+  //   // all input fields
+  //   var $inputs = $group.find(':input');
 
-    // input fields
-    var $first = $form.find('[name=pincode-1]'),
-      $second = $form.find('[name=pincode-2]'),
-      $third = $form.find('[name=pincode-3]'),
-      $fourth = $form.find('[name=pincode-4]');
+  //   // input fields
+  //   var $first = $form.find('[name=pincode-1]'),
+  //     $second = $form.find('[name=pincode-2]'),
+  //     $third = $form.find('[name=pincode-3]'),
+  //     $fourth = $form.find('[name=pincode-4]');
 
-    // submit button
-    var $button = $form.find('#submit-code');
+  //   // submit button
+  //   var $button = $form.find('#submit-code');
 
-    // all fields
-    $inputs
-      .on('keyup', function (event) {
-        var code = event.keyCode || event.which;
+  //   // all fields
+  //   $inputs
+  //     .on('keyup', function (event) {
+  //       var code = event.keyCode || event.which;
 
-        if (code === 9 && !event.shiftKey) {
-          // prevent default event
-          event.preventDefault();
+  //       if (code === 9 && !event.shiftKey) {
+  //         // prevent default event
+  //         event.preventDefault();
 
-          // focus to submit button
-          $('#submit-code').focus();
-        }
-      })
-      .inputmask({
-        mask: '9',
-        placeholder: '',
-        showMaskOnHover: false,
-        showMaskOnFocus: false,
-        clearIncomplete: true,
-        onincomplete: function () {
-          !debug || console.log('inputmask incomplete');
-        },
-        oncleared: function () {
-          var index = $inputs.index(this),
-            prev = index - 1,
-            next = index + 1;
+  //         // focus to submit button
+  //         $('#submit-code').focus();
+  //       }
+  //     })
+  //     .inputmask({
+  //       mask: '9',
+  //       placeholder: '',
+  //       showMaskOnHover: false,
+  //       showMaskOnFocus: false,
+  //       clearIncomplete: true,
+  //       onincomplete: function () {
+  //         !debug || console.log('inputmask incomplete');
+  //       },
+  //       oncleared: function () {
+  //         var index = $inputs.index(this),
+  //           prev = index - 1,
+  //           next = index + 1;
 
-          if (prev >= 0) {
-            // clear field
-            $inputs.eq(prev).val('');
+  //         if (prev >= 0) {
+  //           // clear field
+  //           $inputs.eq(prev).val('');
 
-            // focus field
-            $inputs.eq(prev).focus();
+  //           // focus field
+  //           $inputs.eq(prev).focus();
 
-            // remove last nubmer
-            _pincode.splice(-1, 1)
-          } else {
-            return false;
-          }
+  //           // remove last nubmer
+  //           _pincode.splice(-1, 1)
+  //         } else {
+  //           return false;
+  //         }
 
-          !debug || console.log('[oncleared]', prev, index, next);
-        },
-        onKeyValidation: function (key, result) {
-          var index = $inputs.index(this),
-            prev = index - 1,
-            next = index + 1;
+  //         !debug || console.log('[oncleared]', prev, index, next);
+  //       },
+  //       onKeyValidation: function (key, result) {
+  //         var index = $inputs.index(this),
+  //           prev = index - 1,
+  //           next = index + 1;
 
-          // focus to next field
-          if (prev < 6) {
-            $inputs.eq(next).focus();
-          }
+  //         // focus to next field
+  //         if (prev < 6) {
+  //           $inputs.eq(next).focus();
+  //         }
 
-          !debug || console.log('[onKeyValidation]', index, key, result, _pincode);
-        },
-        onBeforePaste: function (data, opts) {
-          $.each(data.split(''), function (index, value) {
-            // set value
-            $inputs.eq(index).val(value);
+  //         !debug || console.log('[onKeyValidation]', index, key, result, _pincode);
+  //       },
+  //       onBeforePaste: function (data, opts) {
+  //         $.each(data.split(''), function (index, value) {
+  //           // set value
+  //           $inputs.eq(index).val(value);
 
-            !debug || console.log('[onBeforePaste:each]', index, value);
-          });
+  //           !debug || console.log('[onBeforePaste:each]', index, value);
+  //         });
 
-          return false;
-        }
-      });
+  //         return false;
+  //       }
+  //     });
 
-    // first field
-    $('[name=pincode-1]')
-      .on('focus', function (event) {
-        !debug || console.log('[1:focus]', _pincode);
-      })
-      .inputmask({
-        oncomplete: function () {
-          // add first character
-          _pincode.push($(this).val());
+  //   // first field
+  //   $('[name=pincode-1]')
+  //     .on('focus', function (event) {
+  //       !debug || console.log('[1:focus]', _pincode);
+  //     })
+  //     .inputmask({
+  //       oncomplete: function () {
+  //         // add first character
+  //         _pincode.push($(this).val());
 
-          // focus to second field
-          $('[name=pincode-2]').focus();
+  //         // focus to second field
+  //         $('[name=pincode-2]').focus();
 
-          !debug || console.log('[1:oncomplete]', _pincode);
-        }
-      });
+  //         !debug || console.log('[1:oncomplete]', _pincode);
+  //       }
+  //     });
 
-    // second field
-    $('[name=pincode-2]')
-      .on('focus', function (event) {
-        if (!($first.val().trim() !== '')) {
-          // prevent default
-          event.preventDefault();
+  //   // second field
+  //   $('[name=pincode-2]')
+  //     .on('focus', function (event) {
+  //       if (!($first.val().trim() !== '')) {
+  //         // prevent default
+  //         event.preventDefault();
 
-          // reset pincode
-          _pincode = [];
+  //         // reset pincode
+  //         _pincode = [];
 
-          // handle each field
-          $inputs
-            .each(function () {
-              // clear each field
-              $(this).val('');
-            });
+  //         // handle each field
+  //         $inputs
+  //           .each(function () {
+  //             // clear each field
+  //             $(this).val('');
+  //           });
 
-          // focus to first field
-          $first.focus();
-        }
+  //         // focus to first field
+  //         $first.focus();
+  //       }
 
-        !debug || console.log('[2:focus]', _pincode);
-      })
-      .inputmask({
-        oncomplete: function () {
-          // add second character
-          _pincode.push($(this).val());
+  //       !debug || console.log('[2:focus]', _pincode);
+  //     })
+  //     .inputmask({
+  //       oncomplete: function () {
+  //         // add second character
+  //         _pincode.push($(this).val());
 
-          // focus to third field
-          $('[name=pincode-3]').focus();
+  //         // focus to third field
+  //         $('[name=pincode-3]').focus();
 
-          !debug || console.log('[2:oncomplete]', _pincode);
-        }
-      });
+  //         !debug || console.log('[2:oncomplete]', _pincode);
+  //       }
+  //     });
 
-    // third field
-    $('[name=pincode-3]')
-      .on('focus', function (event) {
-        if (!($first.val().trim() !== '' &&
-            $second.val().trim() !== '')) {
-          // prevent default
-          event.preventDefault();
+  //   // third field
+  //   $('[name=pincode-3]')
+  //     .on('focus', function (event) {
+  //       if (!($first.val().trim() !== '' &&
+  //           $second.val().trim() !== '')) {
+  //         // prevent default
+  //         event.preventDefault();
 
-          // reset pincode
-          _pincode = [];
+  //         // reset pincode
+  //         _pincode = [];
 
-          // handle each field
-          $inputs
-            .each(function () {
-              // clear each field
-              $(this).val('');
-            });
+  //         // handle each field
+  //         $inputs
+  //           .each(function () {
+  //             // clear each field
+  //             $(this).val('');
+  //           });
 
-          // focus to first field
-          $first.focus();
-        }
+  //         // focus to first field
+  //         $first.focus();
+  //       }
 
-        !debug || console.log('[3:focus]', _pincode);
-      })
-      .inputmask({
-        oncomplete: function () {
-          // add third character
-          _pincode.push($(this).val());
+  //       !debug || console.log('[3:focus]', _pincode);
+  //     })
+  //     .inputmask({
+  //       oncomplete: function () {
+  //         // add third character
+  //         _pincode.push($(this).val());
 
-          // focus to fourth field
-          $('[name=pincode-4]').focus();
+  //         // focus to fourth field
+  //         $('[name=pincode-4]').focus();
 
-          !debug || console.log('[3:oncomplete]', _pincode);
-        }
-      });
+  //         !debug || console.log('[3:oncomplete]', _pincode);
+  //       }
+  //     });
 
-    // sixth field
-    $('[name=pincode-4]')
-      .on('focus', function (event) {
-        if (!($first.val().trim() !== '' &&
-            $second.val().trim() !== '' &&
-            $third.val().trim() !== '')) {
-          // prevent default
-          event.preventDefault();
+  //   // sixth field
+  //   $('[name=pincode-4]')
+  //     .on('focus', function (event) {
+  //       if (!($first.val().trim() !== '' &&
+  //           $second.val().trim() !== '' &&
+  //           $third.val().trim() !== '')) {
+  //         // prevent default
+  //         event.preventDefault();
 
-          // reset pincode
-          _pincode = [];
+  //         // reset pincode
+  //         _pincode = [];
 
-          // handle each field
-          $inputs
-            .each(function () {
-              // clear each field
-              $(this).val('');
-            });
+  //         // handle each field
+  //         $inputs
+  //           .each(function () {
+  //             // clear each field
+  //             $(this).val('');
+  //           });
 
-          // focus to first field
-          $first.focus();
-        }
+  //         // focus to first field
+  //         $first.focus();
+  //       }
 
-        !debug || console.log('[4:focus]', _pincode);
-      })
-      .inputmask({
-        oncomplete: function () {
-          // add sixth character
-          _pincode.push($(this).val());
+  //       !debug || console.log('[4:focus]', _pincode);
+  //     })
+  //     .inputmask({
+  //       oncomplete: function () {
+  //         // add sixth character
+  //         _pincode.push($(this).val());
 
-          // pin length not equal to six characters
-          if (_pincode.length !== 6) {
-            // reset pin
-            _pincode = [];
+  //         // pin length not equal to six characters
+  //         if (_pincode.length !== 6) {
+  //           // reset pin
+  //           _pincode = [];
 
-            // handle each field
-            $inputs
-              .each(function () {
-                // clear each field
-                $(this).val('');
-              });
+  //           // handle each field
+  //           $inputs
+  //             .each(function () {
+  //               // clear each field
+  //               $(this).val('');
+  //             });
 
-            // focus to first field
-            $('[name=pincode-1]').focus();
-          } else {
-            // handle each field
-            $inputs.each(function () {
-              // disable field
-              $(this).prop('disabled', true);
-            });
+  //           // focus to first field
+  //           $('[name=pincode-1]').focus();
+  //         } else {
+  //           // handle each field
+  //           $inputs.each(function () {
+  //             // disable field
+  //             $(this).prop('disabled', true);
+  //           });
 
-            // send request
-            _req = $.ajax({
-                type: 'POST',
-                url: '/api/tfa',
-                data: {
-                  'code': _pincode.join(''),
-                  '_csrf': ''
-                }
-              })
-              .done(function (data, textStatus, jqXHR) {
-                try {
-                  !debug || console.log('data', data);
+  //           // send request
+  //           _req = $.ajax({
+  //               type: 'POST',
+  //               url: '/api/tfa',
+  //               data: {
+  //                 'code': _pincode.join(''),
+  //                 '_csrf': ''
+  //               }
+  //             })
+  //             .done(function (data, textStatus, jqXHR) {
+  //               try {
+  //                 !debug || console.log('data', data);
 
-                  if (data.ok === true) {
-                    $group.addClass('form__group--success');
-                    $button.removeAttr('disabled');
-                  }
+  //                 if (data.ok === true) {
+  //                   $group.addClass('form__group--success');
+  //                   $button.removeAttr('disabled');
+  //                 }
 
-                  if (data.ok === false) {
-                    $group.addClass('form__group--error');
-                  }
-                } catch (err) {
+  //                 if (data.ok === false) {
+  //                   $group.addClass('form__group--error');
+  //                 }
+  //               } catch (err) {
 
-                }
-              })
-              .fail(function (jqXHR, textStatus, errorThrown) {
-                $group.removeClass('form__group--error');
-              })
-              .always(function (dataOrjqXHR, textStatus, jqXHRorErrorThrown) {
-                // reset pin
-                _pincode = [];
+  //               }
+  //             })
+  //             .fail(function (jqXHR, textStatus, errorThrown) {
+  //               $group.removeClass('form__group--error');
+  //             })
+  //             .always(function (dataOrjqXHR, textStatus, jqXHRorErrorThrown) {
+  //               // reset pin
+  //               _pincode = [];
 
-                // reset request
-                _req = null;
+  //               // reset request
+  //               _req = null;
 
-                setTimeout(function () {
-                  // handle each field
-                  $inputs.each(function () {
-                    // clear all fields
-                    $(this).val('');
+  //               setTimeout(function () {
+  //                 // handle each field
+  //                 $inputs.each(function () {
+  //                   // clear all fields
+  //                   $(this).val('');
 
-                    // enable all fields
-                    $(this).prop('disabled', false);
-                  });
+  //                   // enable all fields
+  //                   $(this).prop('disabled', false);
+  //                 });
 
-                  // remove response status class
-                  $group.removeClass('form__group--success form__group--error');
+  //                 // remove response status class
+  //                 $group.removeClass('form__group--success form__group--error');
 
-                  // disable submit button
-                  $button.attr('disabled', true);
+  //                 // disable submit button
+  //                 $button.attr('disabled', true);
 
-                  // focus to first field
-                  $first.focus();
-                }, 2000);
-              });
-          }
+  //                 // focus to first field
+  //                 $first.focus();
+  //               }, 2000);
+  //             });
+  //         }
 
-          !debug || console.log('[4:oncomplete]', _pincode);
-        }
-      });
-  });
+  //         !debug || console.log('[4:oncomplete]', _pincode);
+  //       }
+  //     });
+  // });
 
 });
-
+$(document).ready(function () {
+  $('.burger').click(function (event) {
+    $('.burger, .mobile-header').toggleClass('activebur');
+    $('body').toggleClass('lock');
+  })
+  $('.links a').click(function (event) {
+    $('.burger, .mobile-header').removeClass('activebur');
+    $('body').removeClass('lock');
+  })
+});
 window.addEventListener('scroll', function () {
   let y = document.documentElement.scrollTop;
   if (y > 100) {
